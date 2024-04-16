f08bbd0557172ccbda506a25222c32fc3b904c9e
Paulo Lucas <paulo.lucas@rocketmail.com>
2024-04-16 10:05:45 -0300

    Criando novas funcionalidades na API Rest

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/autores.js";
 import livro from "../models/livros.js";
 
 class LivroController {
@@ -30,9 +31,13 @@ class LivroController {
 
     static async cadastrarLivro (req, res) {
 
+        const novoLivro = req.body;
+
         try {
 
-            const novoLivro = await livro.create(req.body);
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = {...novoLivro, autor: {...autorEncontrado._doc}};
+            const livroCriado = await livro.create(livroCompleto);
             res.status(201).json({message: "Livro cadastrado com sucesso!", livro: novoLivro});
 
         } catch (erro) {
@@ -68,6 +73,17 @@ class LivroController {
             res.status(500).json({message:`${erro.message} - Falha ao excluir livro!`});
         }
     };
+
+    static async listarLivrosPorEditora (req, res) {
+        const editora = req.query.editora;
+        try {
+            const livrosPorEditora = await livro.find({editora: editora});
+            res.status(200).json(livrosPorEditora);
+        } catch (erro) {
+            res.status(500).json({message:`${erro.message} - Falha ao listar livros por editora!`});
+        }
+      
+    }
 };
 
 export default LivroController;
